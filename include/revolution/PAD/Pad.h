35929a0bfcca6c55c6305a72cc86caef54203c1f
Chloe <228616821+akiramusic000@users.noreply.github.com>
2026-07-26 09:08:10 -0500

    Find bounds for `revolution/KPAD`, fix `revolution/PAD`

--- include/revolution/PAD/Pad.h
+++ include/revolution/PAD/Pad.h
@@ -7,6 +7,31 @@ extern "C" {
 
 extern u32 __PADSpec;
 
+#define PAD_SPEC_0 0
+#define PAD_SPEC_1 1
+#define PAD_SPEC_2 2
+#define PAD_SPEC_3 3
+#define PAD_SPEC_4 4
+#define PAD_SPEC_5 5
+
+#define PAD_MOTOR_STOP      0
+#define PAD_MOTOR_RUMBLE    1
+#define PAD_MOTOR_STOP_HARD 2
+
+#define PAD_CHAN0_BIT 0x80000000
+#define PAD_CHAN1_BIT 0x40000000
+#define PAD_CHAN2_BIT 0x20000000
+#define PAD_CHAN3_BIT 0x10000000
+
+#define PAD_MAX_CONTROLLERS     4
+
+#define PAD_ERR_NONE           0
+#define PAD_ERR_NO_CONTROLLER -1
+#define PAD_ERR_NOT_READY     -2
+#define PAD_ERR_TRANSFER      -3
+
+#define RES_WIRELESS_LITE 0x40000
+
 typedef enum {
     PAD_BUTTON_LEFT = (1 << 0),
     PAD_BUTTON_RIGHT = (1 << 1),
--- src/revolution/PAD/Pad.c
+++ src/revolution/PAD/Pad.c
@@ -1,8 +1,30 @@
 #include <revolution/OS.h>
 #include <revolution/PAD.h>
+#include <revolution/SI.h>
 
-u32 __PADSpec = 0;
-u32 PADEnabledBits = 0;
+static u32 AnalogMode = 0x300;
+static u32 Spec = PAD_SPEC_5;
+static u32 PADEnabledBits;
+
+u32 __PADSpec;
+
+void PADControlMotor(s32 chan, u32 command) {
+    BOOL enabled;
+    u32 chanBit;
+
+    enabled = OSDisableInterrupts();
+    chanBit = PAD_CHAN0_BIT >> chan;
+    if ((PADEnabledBits & chanBit) && !(SIGetType(chan) & 0x20000000 /* SI_GC_NOMOTOR */)) {
+        if (Spec < PAD_SPEC_2 && command == PAD_MOTOR_STOP_HARD)
+            command = PAD_MOTOR_STOP;
+        if (OS_PAD_FLAGS & 0x20)
+            command = PAD_MOTOR_STOP;
+        SISetCommand(chan, (0x40 << 16) | AnalogMode | (command & (0x00000001 | 0x00000002)));
+        SITransferCommands();
+    }
+
+    OSRestoreInterrupts(enabled);
+}
 
 BOOL __PADDisableRecalibration(BOOL disable) {
     BOOL enabled = OSDisableInterrupts();
